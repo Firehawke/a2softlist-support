93e9b3a4d06d2e4766d0c255c1a56b3b7573f471
Firehawke <Firehawke@gmail.com>
2020-03-02 13:53:21 -0700

    Complete the removal...

--- ia2sl.sh
+++ ia2sl.sh
@@ -101,7 +101,6 @@ function generator() {
         echo -e -n '\t\t<info name="release" value="' >>../xml/disk/disk$1.xml
         xmllint --xpath 'metadata/publicdate/text()' $filename | awk '{print $1}' | tr -d '\n' >>../xml/disk/disk$1.xml
         echo -e '"/>' >>../xml/disk/disk$1.xml
-        echo -e -n '\t\t<!--' >>../xml/disk/disk$1.xml
         # Now, this next step only is done if we're doing WOZADAY where we have actual compatibility data at hand.
         case ${2} in
         "WOZADAY")
